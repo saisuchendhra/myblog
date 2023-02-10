5fbb34a3b4bcbe77edde78d3780d7dc7ce32d1e8
saisuchendhra eravathri <saisuchendhra@gmail.com>
2023-02-10 02:09:46 -0700

    small changes

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+# STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
